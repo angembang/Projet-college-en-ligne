d7f4df3091193059f646a941f5b1ba3f91ceb0a6
Ange Mbang <angembang@gmail.com>
2024-04-03 17:45:34 +0000

    updating the AbstractController for adding eender data as JSON response.

--- controllers/AbstractController.php
+++ controllers/AbstractController.php
@@ -1,8 +1,18 @@
 <?php
 
+/**
+ * Abstract controller class providing common functionalities for controllers.
+ */
 abstract class AbstractController
 {
+    /**
+     * @var \Twig\Environment $twig The Twig environment instance.
+     */
     private \Twig\Environment $twig;
+    
+    /**
+     * Constructor method to initialize the Twig environment.
+     */
     public function __construct()
     {
         $loader = new \Twig\Loader\FilesystemLoader('templates');
@@ -10,26 +20,37 @@ abstract class AbstractController
             'debug' => true,
         ]);
 
+        // Enable debugging in Twig environment
         $twig->addExtension(new \Twig\Extension\DebugExtension());
         
+        // Add CSRF token to global Twig variables
         $twig->addGlobal('csrf_token', $_SESSION["csrf-token"]);
 
-        // Ajoutez les messages d'erreur à Twig
+        // Add error messages to global Twig variables
         $twig->addGlobal('error_message', isset($_SESSION["error-message"]) ? 
         $_SESSION["error-message"] : null);
-        //$twig->addGlobal('user', $_SESSION["user"]);
-        // Vérifie si $_SESSION["user"] est défini avant de l'ajouter à Twig
-        //$twig->addGlobal('user', isset($_SESSION["user"]) ? $_SESSION["user"] : null);
         
-
         $this->twig = $twig;
     }
-
+    
+    /**
+     * Render a Twig template with provided data.
+     *
+     * @param string $template The template file path.
+     * @param array $data An array of data to be passed to the template.
+     * @return void
+     */
     protected function render(string $template, array $data) : void
     {
         echo $this->twig->render($template, $data);
     }
     
+    /**
+     * Render data as JSON response.
+     *
+     * @param array $data The data to be encoded as JSON.
+     * @return void
+     */
     protected function renderJson(array $data): void
     {
         header('Content-Type: application/json');
