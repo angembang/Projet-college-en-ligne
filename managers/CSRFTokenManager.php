65034f4b7fc7e2e9fa5544b61dad348f049d93d3
Ange Mbang <angembang@gmail.com>
2024-04-03 17:51:53 +0000

    updating CSRFTokenManager

--- managers/CSRFTokenManager.php
+++ managers/CSRFTokenManager.php
@@ -1,7 +1,17 @@
 <?php
 
+/**
+ * Class CSRFTokenManager
+ *
+ * Manages CSRF tokens for form submission protection.
+ */
 class CSRFTokenManager
 {
+    /**
+     * Generates a CSRF token.
+     *
+     * @return string The generated CSRF token.
+     */
     public function generateCSRFToken(): string
     {
         $token = bin2hex(random_bytes(32));
@@ -10,6 +20,12 @@ class CSRFTokenManager
     }
     
     
+    /**
+     * Validates a CSRF token.
+     *
+     * @param mixed $token The token to validate.
+     * @return bool True if the token is valid, false otherwise.
+     */
     public function validateCSRFToken($token): bool
     {
         if(isset($_POST["csrf-token"]) && hash_equals($_SESSION["csrf-token"], $token))
