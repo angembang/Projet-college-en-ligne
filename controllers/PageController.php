93419f15cf638055de831a78236cfafc7a2cdddf
Ange Mbang <angembang@gmail.com>
2024-04-03 18:05:11 +0000

    updating the PageController

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,12 +1,31 @@
 <?php
 
+/**
+ * Class PageController
+ *
+ * Controller class for managing page-related actions.
+ */
 class PageController extends AbstractController
 {
+     /**
+     * Renders the home page.
+     *
+     * This method is responsible for rendering the home page view.
+     *
+     * @return void
+     */
     public function home(): void
     {
         $this->render("home.html.twig", []);
     }
     
+    /**
+     * Renders the principal home page.
+     *
+     * This method is responsible for rendering the principal home page view.
+     *
+     * @return void
+     */
     public function homePrincipal(): void
     {
         $this->render("superAdminHome.html.twig", []);
