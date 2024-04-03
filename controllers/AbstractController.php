c190cbf350c81b1f961c38a79ff292415f6026ce
Ange Mbang <angembang@gmail.com>
2024-04-03 16:49:18 +0000

    updating the AbstractController of the plateform

--- controllers/AbstractController.php
+++ controllers/AbstractController.php
@@ -1,37 +1,39 @@
 <?php
 
-/**
- * Abstract class defining a base controller for managing Twig views.
- */
 abstract class AbstractController
 {
-    /** @var \Twig\Environment $twig The instance of Twig environment. */
     private \Twig\Environment $twig;
-    
-    /**
-     * Constructor of the AbstractController class.
-     * Initialize the Twig environment with default parameters.
-     */
     public function __construct()
     {
         $loader = new \Twig\Loader\FilesystemLoader('templates');
-        $twig = new \Twig\Environment($loader, [
-            "debug" => true
+        $twig = new \Twig\Environment($loader,[
+            'debug' => true,
         ]);
-        
+
         $twig->addExtension(new \Twig\Extension\DebugExtension());
+        
+        $twig->addGlobal('csrf_token', $_SESSION["csrf-token"]);
+
+        // Ajoutez les messages d'erreur à Twig
+        $twig->addGlobal('error_message', isset($_SESSION["error-message"]) ? 
+        $_SESSION["error-message"] : null);
+        //$twig->addGlobal('user', $_SESSION["user"]);
+        // Vérifie si $_SESSION["user"] est défini avant de l'ajouter à Twig
+        //$twig->addGlobal('user', isset($_SESSION["user"]) ? $_SESSION["user"] : null);
+        
+
         $this->twig = $twig;
     }
-    
-    /**
-     * Render the Twig template with the provided data.
-     *
-     * @param string $template The name of the template to render.
-     * @param array $data The data to pass to the template.
-     * @return void
-     */
-    protected function render(string $template, array $data): void
+
+    protected function render(string $template, array $data) : void
     {
         echo $this->twig->render($template, $data);
     }
+    
+    protected function renderJson(array $data): void
+    {
+        header('Content-Type: application/json');
+        echo json_encode($data);
+    }
+
 }
