abf3446d2483f73f3a6e52e86736b46d844ef9b5
Ange Mbang <angembang@gmail.com>
2024-04-03 17:18:15 +0000

    Initialized session, loaded environment variables, generated CSRF token, and handled requests using Router class in index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+// Start the session
+session_start();
+
 
 // Load the Composer autoloader
 require "vendor/autoload.php";
@@ -6,6 +9,19 @@ require "vendor/autoload.php";
 // Load the contents of the .env file into $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+
+// Check if the CSRF token is not set in the session
+if(!isset($_SESSION["csrf-token"]))
+{
+    // Generate a CSRF token
+    $tokenManager = new CSRFTokenManager();
+    $token = $tokenManager->generateCSRFToken();
+
+    // Store the CSRF token in the session
+    $_SESSION["csrf-token"] = $token;
+}
+
 
 /**
  * @var Router $router an instance of the Router class for handling requests.
@@ -16,3 +32,5 @@ $router = new Router();
  * Handle the request based on the $_GET parameters.
  */
 $router->handleRequest($_GET);
+
+
